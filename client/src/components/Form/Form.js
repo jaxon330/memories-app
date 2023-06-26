02d6aa06eeb6cef1c5e100c458fd840e82ba4fb1
john-330 <jaxon330@gmail.com>
2023-06-26 12:43:23 -0500

    Redux completed

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
+
+
 import useStyles from './styles'
 
 
 const Form = () => {
     const classes = useStyles();
+
+    const handleSubmit = () => {
+        
+    }
     return (
-        <>
-            <h1>Form</h1>
+        <Paper className={classes.paper}>
+            <form autoComplete='off' noValidate className={classes.form} onSubmit={handleSubmit}>
+
+            </form>
             
-        </>
+        </Paper>
     )
 }
 
--- server/index.js
+++ server/index.js
@@ -7,11 +7,12 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://jaxon330:0000@cluster0.i2chyjp.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
