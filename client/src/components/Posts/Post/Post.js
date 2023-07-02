e38ac961fdd62412645b703716dbe8d59df58288
john-330 <jaxon330@gmail.com>
2023-07-02 10:00:35 -0500

    delete post functionality was created

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -6,9 +6,14 @@ import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import moment from 'moment'
 
 import useStyles from './styles'
+import { useDispatch } from 'react-redux';
+
+import { deletePost } from '../../../actions/posts';
 
 const Post = ({post, setCurrentId}) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
   return (
     <Card className={classes.card}>
       <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
@@ -24,8 +29,9 @@ const Post = ({post, setCurrentId}) => {
       <div className={classes.details}>
         <Typography variant='body2' color='textSecondary'>{post.tags.map((tag) => `#${tag} `)}</Typography>
       </div>
+      <Typography className={classes.title} variant='h5' gutterBottom >{post.title}</Typography>
       <CardContent>
-        <Typography className={classes.title} variant='h5' gutterBottom >{post.message}</Typography>
+        <Typography variant='h5' gutterBottom >{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
         <Button size='small' color='primary' onClick={() => {}}>
@@ -33,7 +39,7 @@ const Post = ({post, setCurrentId}) => {
             Like
             {post.likeCount}
         </Button>
-        <Button size='small' color='primary' onClick={() => {}}>
+        <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
             <DeleteIcon fontSize='small' />
             Delete
         </Button>
