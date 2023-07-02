4c18258e0cc00cfea7e84bd3650152de4cc9367f
john-330 <jaxon330@gmail.com>
2023-07-02 12:14:53 -0500

    fixed some bugs and started deploying

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -19,7 +19,7 @@ const Post = ({post, setCurrentId}) => {
       <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
       <div className={classes.overlay}>
         <Typography variant='h6'>{post.creator}</Typography>
-        <Typography variant='body2'>{moment(post.createdAt).fromNow}</Typography>
+        <Typography variant='body2'>{moment(post.createdAt).fromNow()}</Typography>
       </div>
       <div className={classes.overlay2}>
         <Button style={{color: 'white'}} size='small' onClick={() => {setCurrentId(post._id)}}>
@@ -31,12 +31,12 @@ const Post = ({post, setCurrentId}) => {
       </div>
       <Typography className={classes.title} variant='h5' gutterBottom >{post.title}</Typography>
       <CardContent>
-        <Typography variant='h5' gutterBottom >{post.message}</Typography>
+        <Typography variant='body2' color='textSecondary' component='p' >{post.message}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
         <Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}>
             <ThumbUpAltIcon fontSize='small' />
-            Like
+            &nbsp; Like &nbsp;
             {post.likeCount}
         </Button>
         <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
