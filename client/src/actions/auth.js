dbf9f84b42c890aaa2c637b1eedcf5fc86982e4d
john-330 <jaxon330@gmail.com>
2023-07-31 23:06:34 -0500

    user action completed

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,7 @@
 import { AUTH } from '../constants/actionTypes'
 import * as api from '../api';
 
+
 export const signin = (formData, navigate) => async (dispatch) => {
     try {
         const { data } = await api.signIn(formData);
@@ -16,7 +17,7 @@ export const signin = (formData, navigate) => async (dispatch) => {
 export const signup = (formData, navigate) => async (dispatch) => {
     try {
         const { data } = await api.signUp(formData);
-
+        console.log(data);
         dispatch({ type: AUTH, data })
 
         navigate('/')
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -8,8 +8,8 @@ const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', auth, createPost);
-router.patch('/:id', auth, updatePost)
-router.delete('/:id', auth, deletePost)
-router.patch('/:id/likePost', auth, likePost)
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
 
 export default router
